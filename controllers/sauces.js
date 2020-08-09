ace260df9df2022be84e47936e76a378f840351f
Axel COCHELIN <ax.cochelin@gmail.com>
2020-08-09 10:28:48 +0200

    Fix problème affichage sauce unique et fix  validateur mail et password

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -8,7 +8,7 @@ exports.getAllSauces = (req, res, next) => {
 };
 
 exports.getOneSauce = (req, res, next) => {
-  Sauce.findOne()
+  Sauce.findOne({ _id: req.params.id })
     .then((sauce) => res.status(200).json(sauce))
     .catch((error) => res.status(404).json({ error }));
 };
--- controllers/user.js
+++ controllers/user.js
@@ -1,79 +1,87 @@
-const bcrypt = require("bcrypt"); //importe bcrypt
-const jsonWebToken = require("jsonwebtoken"); //importe jsonwebtoken
-const User = require("../models/user"); //importe le modèle User
-const emailValidator = require("email-validator"); //importe le validateur d'email
-const passwordValidator = require("password-validator"); //importe le validateur de mot de passe
+const bcrypt = require("bcrypt");
+const jsonWebToken = require("jsonwebtoken");
+const User = require("../models/user");
+const emailValidator = require("email-validator");
+const passwordValidator = require("password-validator");
 
-let validPassword = new passwordValidator(); //crée une variable pour paramétrer le validateur de mot de passe
+// Create a schema
+var validPassword = new passwordValidator();
 
 validPassword
   .is()
-  .min(8) // Longueur mini de 8 caractères
+  .min(8) // longueur mini 8
   .is()
-  .max(20) // Longueur maxi de 20 caractères
+  .max(20) // longueur maxi 20
   .has()
-  .uppercase() // Doit contenir une majuscule
+  .uppercase() // doit contenir une majuscule
   .has()
-  .lowercase() // Doit contenir une minuscule
+  .lowercase() // doit contenir une minuscule
   .has()
-  .digits() // Doit contenir un chiffre
+  .digits() // doit contenir un chiffre
   .has()
   .not()
-  .spaces(); // Ne peut pas contenir d'espaces
+  .spaces(); // ne doit pas contenir d'espace
 
 exports.signup = (req, res, next) => {
   if (
-    !emailValidator.validate(req.body.eamil) ||
-    !validPassword.validate(req.body.password) //si l'adresse mail ou le mot de passe ne sont pas valide
+    !emailValidator.validate(req.body.email) ||
+    !validPassword.validate(req.body.password)
   ) {
-    throw {
-      error:
-        "Merci de bien vouloir entrer une adresse email et un mot de passe valide !", //on renvoit une erreur
-    };
+    throw { error: " entrée invalide !" };
   } else if (
     emailValidator.validate(req.body.email) &&
-    validPassword.validate(req.body.password) //si l'adresse mail et le mot de passe sont valide
+    validPassword.validate(req.body.password)
   ) {
+    //appeler la fonction de hachage, créer un nouvel utilisateur, le sauvegarder dans la base de données
     bcrypt
-      .hash(req.body.password, 10) //on appelle la fonction de hachage
+      .hash(req.body.password, 10)
       .then((hash) => {
         const user = new User({
           email: req.body.email,
           password: hash,
-        }); //on crée un nouvel utilisateur
+        });
         user
-          .save() //on sauvegarde le nouvel utilisateur dans la base de données
-          .then(res.status(201).json({ message: "Nouvel utilisateur créé !" })) // on renvoie une réponse positive
-          .catch((error) => res.status(400).json({ error })); // si problème: on renvoie une erreur
+          .save()
+          .then(res.status(201).json({ message: "Nouvel utilisateur créé !" }))
+          .catch((error) => res.status(400).json({ error }));
       })
-      .catch((error) => res.status(500).json({ error })); // si problème serveur : on renvoie une erreur
+      .catch((error) => res.status(500).json({ error }));
   }
 };
 
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email }) //trouver l'utilisateur qui correspond à l'adresse mail unique
+  /**trouver l'utilisateur qui correspond à l'adresse mail unique
+   * si non trouvé renvoyer une erreur
+   * si trouvé comparer le mot de passe haché avec celui de la base de données grâce à bcrypt
+   * si mot de passe non valide renvoyer une erreur
+   * si valide, valider la requête, renvoyer un userId et un token
+   * si erreur autre renvoyer une erreur serveur
+   */
+
+  User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-        return res.status(401).json({ message: "Utilisateur non trouvé !" }); //si non trouvé renvoyer une erreur
+        return res.status(401).json({ message: "Utilisateur non trouvé !" });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              return res
+                .status(401)
+                .json({ message: "Mot de passe incorrect !" });
+            }
+            res.status(200).json({
+              userId: user._id,
+              token: jsonWebToken.sign(
+                { userId: user._id },
+                "biit,?^OeTv@Dfr68LI%L.l$?U7{2qfZ75.LXO_Udz.D'$@0(p2rfAg4{FZG",
+                { expiresIn: "24h" }
+              ),
+            });
+          })
+          .catch((error) => res.status(500).json({ error }));
       }
-      bcrypt
-        .compare(req.body.password, user.password) //si trouvé comparer le mot de passe haché avec celui de la base de données grâce à bcrypt
-        .then((valid) => {
-          if (!valid) {
-            return res
-              .status(401)
-              .json({ message: "Mot de passe incorrect !" }); //si mot de passe non valide renvoyer une erreur
-          }
-          res.status(200).json({
-            userId: user._id,
-            token: jsonWebToken.sign(
-              { userId: user._id },
-              "biit,?^OeTv@Dfr68LI%L.l$?U7{2qfZ75.LXO_Udz.D'$@0(p2rfAg4{FZG",
-              { expiresIn: "24h" }
-            ), //si valide, valider la requête, renvoyer un userId et un token
-          });
-        })
-        .catch((error) => res.status(500).json({ error })); //si erreur autre renvoyer une erreur serveur
     })
-    .catch((error) => res.status(500).json({ error })); //si erreur autre renvoyer une erreur serveur
+    .catch((error) => res.status(500).json({ error }));
 };
